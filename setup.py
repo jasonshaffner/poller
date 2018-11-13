8b8479ad9394688f2e10ef7a5dec93531225a1e1
Jason Shaffner <jason.shaffner@charter.com>
2018-11-13 13:41:11 -0700

    added several polling scripts

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from distutils.core import setup
 
 setup(name='poller',
-	version='0.3',
+	version='0.5',
 	packages=['poller'],
-    scripts=['scripts/pollbase', \
-            'scripts/pollmodel', \
+    scripts=['scripts/poll', \
+            'scripts/pollbase', \
+            'scripts/pollbulk', \
+            'scripts/pollcontact', \
             'scripts/pollinterfaces', \
+            'scripts/polllocation', \
+            'scripts/pollmodel', \
+            'scripts/pollname', \
+            'scripts/walk', \
             ],
 	)
 
